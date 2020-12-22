f21706a466443e32f7bfe4d69a02730befc4adbd
alyssaurbanski23 <831554@mystma.org>
2020-12-22 16:10:13 +0000

    unit test try 1

--- main.py
+++ main.py
@@ -1,1 +1,19 @@
-#WRITE YOUR CODE IN THIS FILE
+def add(Alyssa):
+    return (Alyssa)
+
+print ("Alyssa")
+
+def add(purple, thirteen):
+    return (purple, thirteen)
+
+print ("purple, thirteen")
+
+def bigadd(x, y, z, a, b):
+    return (x + y + z + a + b )
+
+print (add(2, 3, 5, 4, 1))
+
+def add(x, y, z, a, b):
+    return ((x + y) - (z * a) - b)
+
+print (add(2, 5, 1, 9, 5))
